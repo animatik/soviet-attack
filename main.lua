fe4f6822ef81c3f8e12ab632ea9bdbe41b538a3a
animatik.2012 <animatik.2012@yandex.ru>
2013-09-21 22:05:55 +0400

    Simple windows added

--- main.lua
+++ main.lua
@@ -1,15 +1,8 @@
 --This is just a test file.
-
-wmaker = require "wmaker"
+require "wmaker"
 
 function love.load()
-   win1 = wmaker.window('hello world',200,300,50,50)
-   win1.border_color = {255,0,0,255}
-   win1.window_color = {255,112,10,245}
-   wmaker.addRow(win1,'image')
-   wmaker.addRow(win1,'text')
-   wmaker.addImage(win1,'image','img.png')
-   wmaker.addText(win1,'text','hello world!',36,'furore.otf','center')
+   window = newWindow('   hello world!',300,400,100,100,{128,50,50,255},{235,0,0,255},{255,230,0,255},2,12,'furore.otf')
 end
 
 function love.update(dt)
@@ -17,5 +10,5 @@ function love.update(dt)
 end
 
 function love.draw()
-   wmaker.drawWindow(win1)
+   drawWindow(window)
 end
--- wmaker.lua
+++ wmaker.lua
@@ -1,21 +1,50 @@
-function newWindow(title_of_window,width_of_window,height_of_window,x_of_window,y_of_window)
+function newWindow(title_of_window,width_of_window,height_of_window,x_of_window,y_of_window,bc,wc,tc,sb,sf,fnt)
    window = {
-      title = title_of_window
-      width = width_of_window
-      height = height_of_window
-      x = x_of_window
-      y = y_of_window
-      border_color = {0,0,0,255}
-      border_color = {12,12,12,255}
-      rows = {}
+      title = title_of_window,
+      width = width_of_window,
+      height = height_of_window,
+      x = x_of_window,
+      y = y_of_window,
+      border_color = bc,
+      window_color = wc,
+      title_color = tc,
+      border_size = sb,
+      font_size = sf,
+      font = love.graphics.newFont(fnt,sf),
+      rows = {},
       draging = {
          active = false,
          diffx = 0,
          diffy = 0
       }
    }
+   return window
 end
 
 function addRow(win,name_of_row)
-   table.insert(win.rows,{name,element})
+   table.insert(win.rows,{name,elements})
+   win.rows[#win.rows].name = name_of_row
+   win.rows[#win.rows].elements = {}
+end
+
+function addText(win,name_of_row,name_of_element,text_of_text,sz,fnt)
+end
+
+function drawWindow(win)
+   local lastx
+   lastx = win.x
+   love.graphics.setColor(win.border_color[1],win.border_color[2],win.border_color[3],win.border_color[4])
+   love.graphics.rectangle('fill',win.x-win.border_size,win.y-win.border_size,win.width+win.border_size*2,win.height+win.border_size*2)
+   love.graphics.setColor(win.window_color[1],win.window_color[2],win.window_color[3],win.window_color[4])
+   love.graphics.rectangle('fill',win.x,win.y,win.width,win.height)
+   love.graphics.setColor(win.border_color[1],win.border_color[2],win.border_color[3],win.border_color[4])
+   love.graphics.setLine(win.border_size)
+   love.graphics.line(win.x,win.y+win.font_size,win.x+win.width,win.y+win.font_size)
+   love.graphics.line(win.x+win.font_size,win.y,win.x+win.font_size,win.y+win.font_size)
+   love.graphics.setLine(win.border_size/2)
+   love.graphics.line(win.x+win.font_size/5,win.y+win.font_size/5,win.x+win.font_size-win.font_size/5,win.y+win.font_size-win.font_size/5)
+   love.graphics.line(win.x+win.font_size-win.font_size/5,win.y+win.font_size/5,win.x+win.font_size/5,win.y+win.font_size-win.font_size/5)
+   love.graphics.setFont(win.font)
+   love.graphics.setColor(win.title_color[1],win.title_color[2],win.title_color[3],win.title_color[4])
+   love.graphics.print(win.title,win.x+win.font_size,win.y)
 end
